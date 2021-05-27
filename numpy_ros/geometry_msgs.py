d9d872904d75acc0281bf22582f9c5fec8736b18
Lucas Wolf <iamlucaswolf@gmail.com>
2021-05-27 23:55:38 +0200

    Add kinematics (Accel, Twist, Wrench + stamped versions) conversions (untested).

--- numpy_ros/geometry_msgs.py
+++ numpy_ros/geometry_msgs.py
@@ -4,11 +4,14 @@
 
 import numpy as np
 
-from numpy_ros.conversions import converts_to_numpy, converts_to_message
+from numpy_ros.conversions import converts_to_numpy, converts_to_message, to_numpy
 
 try:
     from geometry_msgs.msg import (
-        Vector3, Vector3Stamped, Point, Point32, PointStamped
+        Accel, AccelStamped, AccelWithCovariance, AccelWithCovarianceStamped,
+        Point, Point32, PointStamped, Twist, TwistStamped, TwistWithCovariance,
+        TwistWithCovarianceStamped, Vector3, Vector3Stamped,  Wrench, 
+        WrenchStamped
     )
 
 except ImportError:
@@ -16,16 +19,42 @@ except ImportError:
         'Could not import geometry_msgs. Is the ROS package installed?'
     )
 
+_stamped_type_to_attr = {
+    AccelStamped: 'accel',
+    AccelWithCovarianceStamped: 'accel',
+    PointStamped: 'point',
+    TwistStamped: 'twist',
+    TwistWithCovarianceStamped: 'twist',
+    Vector3Stamped: 'vector',
+    WrenchStamped: 'wrench',
+}
+
+
+def _unstamp(message):
+    """Unstamps a given message."""
+    attr_name = _stamped_type_to_attr.get(message.__class__)
+    
+    if attr_name:
+        message = getattr(message, attr_name)
+
+    return message
+
+
+def _assert_is_castable(array, dtype):
+    """Checks that every element of the array can be converted to the desired
+    dtype, without loss of precision."""
+
+    min_dtype = np.min_scalar_type(array)
+
+    if not np.can_cast(min_dtype, dtype):
+        raise TypeError(f'Cannot safely cast array {array} to dtype {dtype}.')
+
 
 @converts_to_numpy(Vector3, Vector3Stamped, Point, PointStamped, Point32)
 def vector_to_numpy(message, homogeneous=False):
     """Converts 3d vector mesage types to a flat array."""
 
-    if isinstance(message, Vector3Stamped):
-        message = message.vector
-
-    elif isinstance(message, PointStamped):
-        message = message.point
+    message = _unstamp(message)
 
     data = [message.x, message.y, message.z]
 
@@ -47,15 +76,8 @@ def numpy_to_vector(message_type, array):
             f'Expected array of shape (3,) or (4,), received {array.shape}.'
         )
 
-    # TODO we will probably need this elsewhere...
-    # Check that every element of the array can be converted to the desired
-    # dtype, without losing precision
-
     dtype = np.float32 if message_type is Point32 else np.float64
-    min_dtype = np.min_scalar_type(array)
-
-    if not np.can_cast(min_dtype, dtype):
-        raise TypeError(f'Cannot safely cast array {array} to dtype {dtype}.')
+    _assert_is_castable(array, dtype)
     
     if len(array) == 4 and not np.isclose(array[3], 1.0):
         raise ValueError(
@@ -64,3 +86,107 @@ def numpy_to_vector(message_type, array):
         )
 
     return message_type(*array[:3])
+
+
+@converts_to_numpy(
+    Accel, 
+    AccelStamped, 
+    Twist, 
+    TwistStamped, 
+    Wrench, 
+    WrenchStamped
+)
+def kinematics_to_numpy(message, homogeneous=False):
+
+    message = _unstamp(message)
+
+    is_wrench = isinstance(message, Wrench)
+
+    linear_message = message.force if is_wrench else message.linear
+    angular_message = message.torque if is_wrench else message.angular
+
+    linear = vector_to_numpy(linear_message, homogeneous=homogeneous)
+    angular = vector_to_numpy(angular_message, homogeneous=homogeneous)
+
+    return linear, angular
+
+
+@converts_to_message(Accel, Twist, Wrench)
+def numpy_to_kinamatics(message_type, linear, angular):
+
+    is_wrench = message_type is Wrench
+
+    linear_key = 'force' if is_wrench else 'linear'
+    angular_key = 'torque' if is_wrench else 'angular'
+
+    kwargs = {
+        linear_key: numpy_to_vector(linear),
+        angular_key: numpy_to_vector(angular)
+    }
+
+    return message_type(**kwargs)
+
+
+@converts_to_numpy(
+    AccelWithCovariance,
+    AccelWithCovarianceStamped,
+    TwistWithCovariance,
+    TwistWithCovarianceStamped,
+)
+def kinematics_with_covariance_to_numpy(message, homogeneous=False):
+    
+    message = _unstamp(message)
+
+    is_accel = isinstance(message, AccelWithCovariance)
+    kinematics_message = message.accel if is_accel else message.twist
+
+    linear, angular = kinematics_to_numpy(
+        kinematics_message, 
+        homogeneous=homogeneous
+    )
+
+    covariance = np.array(message.covariance, dtype=np.float64).reshape(6, 6)
+
+    return linear, angular, covariance
+
+
+@converts_to_message(AccelWithCovariance, TwistWithCovariance)
+def numpy_to_kinematics_with_covariance(
+    message_type, 
+    linear, 
+    angular, 
+    covariance
+    ):
+
+    is_accel = message_type is AccelWithCovariance
+    
+    kinematics_key = 'accel' if is_accel else 'twist'
+    kinematics_message_type = Accel if is_accel else Twist
+
+    kinematics_message = numpy_to_kinamatics(
+        kinematics_message_type, 
+        linear,
+        angular
+    )
+
+    covariance_message = numpy_to_covariance(covariance)
+
+    kwargs = {
+        kinematics_key: kinematics_message,
+        'covariance': covariance_message
+    }
+
+    return message_type(**kwargs)
+
+
+def numpy_to_covariance(array):
+
+    _assert_is_castable(array, np.float64)
+
+    if array.shape != (6,6):
+        raise ValueError(
+            (f'Expected covariance matrix of shape (6,6), received '
+             f'{array.shape}.')
+        )
+
+    return tuple(array.flatten())
