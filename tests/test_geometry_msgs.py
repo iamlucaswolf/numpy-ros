7c6fafbd88c922369b9d900b97dfb30ee46888e8
Lucas Wolf <iamlucaswolf@gmail.com>
2021-06-03 22:25:37 +0200

    Add tests for kinematics with covariance; minor refactorings

--- tests/test_geometry_msgs.py
+++ tests/test_geometry_msgs.py
@@ -15,43 +15,78 @@ from numpy_ros import to_numpy, to_message
 
 
 @pytest.fixture()
-def vector3():
+def vector():
+    return np.array([1.0, 2.0, 3.0])
+
+
+@pytest.fixture()
+def covariance():
+    return np.arange(36, dtype=np.float64).reshape(6, 6)
+
+
+@pytest.fixture()
+def inertia_tensor():
+    return np.array([[1.0, 2.0, 3.0], [2.0, 4.0, 5.0], [3.0, 5.0, 6.0]])
+
+@pytest.fixture()
+def vector3_msg():
     return Vector3(1.0, 2.0, 3.0)
 
 
 @pytest.fixture()
-def accel(vector3):
-    return Accel(vector3, vector3)
+def accel_msg(vector3_msg):
+    return Accel(vector3_msg, vector3_msg)
 
 
 @pytest.fixture()
-def accel_stamped(accel):
-    return AccelStamped(accel=accel)
+def accel_stamped_msg(accel_msg):
+    return AccelStamped(accel=accel_msg)
 
 
 @pytest.fixture()
-def twist(vector3):
-    return Twist(vector3, vector3)
+def twist_msg(vector3_msg):
+    return Twist(vector3_msg, vector3_msg)
 
 
 @pytest.fixture()
-def twist_stamped(twist):
-    return TwistStamped(twist=twist)
+def twist_stamped_msg(twist_msg):
+    return TwistStamped(twist=twist_msg)
 
 
 @pytest.fixture()
-def wrench(vector3):
-    return Wrench(vector3, vector3)
+def wrench_msg(vector3_msg):
+    return Wrench(vector3_msg, vector3_msg)
 
 
 @pytest.fixture
-def wrench_stamped(wrench):
-    return WrenchStamped(wrench=wrench)
+def wrench_stamped_msg(wrench_msg):
+    return WrenchStamped(wrench=wrench_msg)
 
 
-@pytest.fixture()
-def vector_array():
-    return np.array([1.0, 2.0, 3.0])
+@pytest.fixture
+def accel_with_covariance_msg(accel_msg):
+    return AccelWithCovariance(accel_msg, tuple(float(x) for x in range(36)))
+
+
+@pytest.fixture
+def accel_with_covariance_stamped_msg(accel_with_covariance_msg):
+    return AccelWithCovarianceStamped(accel=accel_with_covariance_msg)
+
+
+@pytest.fixture
+def twist_with_covariance_msg(twist_msg):
+    return TwistWithCovariance(twist_msg, tuple(float(x) for x in range(36)))
+
+
+@pytest.fixture
+def twist_with_covariance_stamped_msg(twist_with_covariance_msg):
+    return TwistWithCovarianceStamped(twist=twist_with_covariance_msg)
+
+
+@pytest.fixture
+def inertia_msg(vector3_msg):
+    return Inertia(m=0.0, com=vector3_msg, ixx=1.0, ixy=2.0, ixz=3.0, iyy=4.0,
+        iyz=5.0, izz=6.0)
 
 
 def array_equal(array, other):
@@ -61,53 +96,53 @@ def array_equal(array, other):
 ## Accel, AccelStamped
 ##
 
-def test_accel_to_numpy(accel, vector_array):
-    linear, angular = to_numpy(accel)
+def test_accel_to_numpy(accel_msg, vector):
+    linear, angular = to_numpy(accel_msg)
 
-    assert array_equal(linear, vector_array)
-    assert array_equal(angular, vector_array)
-
-
-def test_accel_stamped_to_numpy(accel_stamped, vector_array):
-    test_accel_to_numpy(accel_stamped.accel, vector_array)
+    assert array_equal(linear, vector)
+    assert array_equal(angular, vector)
 
 
-def test_numpy_to_accel(vector_array, vector3):
-    message = to_message(Accel, vector_array, vector_array)
+def test_accel_stamped_to_numpy(accel_stamped_msg, vector):
+    test_accel_to_numpy(accel_stamped_msg.accel, vector)
+
+
+def test_numpy_to_accel(vector, vector3_msg):
+    message = to_message(Accel, vector, vector)
 
     assert isinstance(message, Accel)
     
     assert isinstance(message.linear, Vector3)
-    assert message.linear == vector3
+    assert message.linear == vector3_msg
     
     assert isinstance(message.angular, Vector3)
-    assert message.angular == vector3
+    assert message.angular == vector3_msg
 
 ##
 ## Twist, TwistStamped
 ##
 
-def test_twist_to_numpy(twist, vector_array):
-    linear, angular = to_numpy(twist)
+def test_twist_to_numpy(twist_msg, vector):
+    linear, angular = to_numpy(twist_msg)
 
-    assert array_equal(linear, vector_array)
-    assert array_equal(angular, vector_array)
-
-
-def test_twist_stamped_to_numpy(twist_stamped, vector_array):
-    test_twist_to_numpy(twist_stamped.twist, vector_array)
+    assert array_equal(linear, vector)
+    assert array_equal(angular, vector)
 
 
-def test_numpy_to_twist(vector_array, vector3):
-    message = to_message(Twist, vector_array, vector_array)
+def test_twist_stamped_to_numpy(twist_stamped_msg, vector):
+    test_twist_to_numpy(twist_stamped_msg.twist, vector)
+
+
+def test_numpy_to_twist(vector, vector3_msg):
+    message = to_message(Twist, vector, vector)
 
     assert isinstance(message, Twist)
     
     assert isinstance(message.linear, Vector3)
-    assert message.linear == vector3
+    assert message.linear == vector3_msg
     
     assert isinstance(message.angular, Vector3)
-    assert message.angular == vector3
+    assert message.angular == vector3_msg
 
 
 ##
@@ -115,27 +150,80 @@ def test_numpy_to_twist(vector_array, vector3):
 ##
 
 
-def test_wrench_to_numpy(wrench, vector_array):
-    force, torque = to_numpy(wrench)
+def test_wrench_to_numpy(wrench_msg, vector):
+    force, torque = to_numpy(wrench_msg)
 
-    assert array_equal(force, vector_array)
-    assert array_equal(torque, vector_array)
-
-
-def test_wrench_stamped_to_numpy(wrench_stamped, vector_array):
-    test_wrench_to_numpy(wrench_stamped.wrench, vector_array)
+    assert array_equal(force, vector)
+    assert array_equal(torque, vector)
 
 
-def test_numpy_to_wrench(vector_array, vector3):
-    message = to_message(Wrench, vector_array, vector_array)
+def test_wrench_stamped_to_numpy(wrench_stamped_msg, vector):
+    test_wrench_to_numpy(wrench_stamped_msg.wrench, vector)
+
+
+def test_numpy_to_wrench(vector, vector3_msg):
+    message = to_message(Wrench, vector, vector)
 
     assert isinstance(message, Wrench)
     
     assert isinstance(message.force, Vector3)
-    assert message.force == vector3
+    assert message.force == vector3_msg
     
     assert isinstance(message.torque, Vector3)
-    assert message.torque == vector3
+    assert message.torque == vector3_msg
+
+
+##
+## AccelWithCovariance, AccelWithCovarianceStamped
+##
+
+def test_accel_with_covariance_to_numpy(accel_with_covariance_msg, covariance):
+    linear, _, covariance_ = to_numpy(accel_with_covariance_msg)
+
+    test_accel_to_numpy(accel_with_covariance_msg.accel, linear)
+    assert array_equal(covariance, covariance_)
+
+
+def test_accel_with_covariance_stamped_to_numpy(accel_with_covariance_stamped_msg, covariance):
+    test_accel_with_covariance_to_numpy(accel_with_covariance_stamped_msg.accel, covariance)
+
+
+def test_numpy_to_accel_with_covariance(vector, accel_msg, covariance):
+    message = to_message(AccelWithCovariance, vector, vector, covariance)
+
+    assert isinstance(message, AccelWithCovariance)
+
+    assert isinstance(message.accel, Accel)
+    assert message.accel == accel_msg
+
+    assert array_equal(np.array(message.covariance).reshape(6,6), covariance)
+
+
+##
+## TwistWithCovariance, TwistWithCovarianceStamped
+##
+
+def test_twist_with_covariance_to_numpy(twist_with_covariance_msg, covariance):
+    linear, _, covariance_ = to_numpy(twist_with_covariance_msg)
+
+    test_twist_to_numpy(twist_with_covariance_msg.twist, linear)
+    assert array_equal(covariance, covariance_)
+
+
+def test_twist_with_covariance_stamped_to_numpy(twist_with_covariance_stamped_msg, covariance):
+    test_twist_with_covariance_to_numpy(twist_with_covariance_stamped_msg.twist, covariance)
+
+
+def test_numpy_to_twist_with_covariance(vector, twist_msg, covariance):
+    message = to_message(TwistWithCovariance, vector, vector, covariance)
+
+    assert isinstance(message, TwistWithCovariance)
+
+    assert isinstance(message.twist, Twist)
+    assert message.twist == twist_msg
+
+    assert array_equal(np.array(message.covariance).reshape(6,6), covariance)
+
 
 
 def test_quaternion_to_numpy():
